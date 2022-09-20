9a32038fd911c253c0d9d4a1df0c7d96102b5130
osvaldoleiva <osvaldo.leiva@mercadolibre.cl>
2022-09-20 02:09:39 -0300

    grandes cambios lpm

--- admin/admin/js/depto.js
+++ admin/admin/js/depto.js
@@ -1,43 +1,22 @@
 var fotoDeptoList = [];
 var selectCondicionesDeUso;
 var condicionesDeUso = [];
-var listCondicionesAgregadas = []
-
+var selectServiciosDepto;
+var serviciosDepto= [];
+var listCondicionesAgregadas = [];
+var listServiciosAgregadosDepto = [];
+var listCondicionesAgregadosObject = [];
+var listServiciosAgregadosObject = [];
+//
 var form = document.getElementById('crear-depto-form');
 form.addEventListener('submit', async function(e){
     e.preventDefault();
-
-    var fotoDeptoList = [];
-    // cargar imagen
-    const userFile = document.getElementById('formFile').files[0];
-    var image64 = await toBase64(userFile);
-
     var formData = new FormData(form);
-
-    var fotoEnviar = {
-        "tituloFotoDepto": "tituloFotoDepto",
-        "fotoDepto": image64
-    }
-
-    fotoDeptoList.push(fotoEnviar);
-    
     // añadiendo lista de serivicion a object plain 
     const plainObjectFormData = Object.fromEntries(formData.entries());
-    const listaServicios = [
-        {
-            "idServicioDepto":3
-        }
-    ]
-    const listaCondiciones = [
-        {
-            "idCondicion":3
-        }
-    ]
-    plainObjectFormData.servicioDeptoList = listaServicios;
     plainObjectFormData.fotoDeptoList = fotoDeptoList
-    plainObjectFormData.condicionesDeUsoList = listaCondiciones
-
-    console.log(JSON.stringify(plainObjectFormData))
+    plainObjectFormData.servicioDeptoList = listServiciosAgregadosObject
+    plainObjectFormData.condicionesDeUsoList = listCondicionesAgregadosObject
     
     fetch("http://localhost:8085/departamento/crear-depto",
     {
@@ -64,6 +43,8 @@ var toBase64 = file => new Promise((resolve, reject) => {
     reader.onerror = error => reject(error);
 });
 
+
+//----------CONDICIONES DE
 //espera que la pagina este totalmente cargada y ejecuta la funcion
 //window hace referencia a la pagina (html),
 window.addEventListener('load', async (event) => {
@@ -85,6 +66,7 @@ window.addEventListener('load', async (event) => {
     }
 });
 
+
 var btnCondicion = document.getElementById("btn-agregar-condicion");
 var listadoCondicionesSeleccionadas = document.getElementById("list-condiciones");
 btnCondicion.addEventListener('click', function(){
@@ -92,15 +74,92 @@ btnCondicion.addEventListener('click', function(){
     var value = selectCondicionesDeUso.value;
     // guarda el texto seleccionado del select
     var textoSeleccionado = selectCondicionesDeUso.options[selectCondicionesDeUso.selectedIndex].text;
-    text = "prohibdo fumar"
+    //text = "prohibdo fumar"
     var divPadreCondicion = document.createElement("div")
     var spanTituloCondicion = document.createElement("span")
     
-    if(text !== "Seleccione condicion" && !listCondicionesAgregadas.includes(textoSeleccionado)) {
-        listCondicionesAgregadas.push(textoSeleccionado);
+    if(textoSeleccionado !== "Seleccione condicion" && !listCondicionesAgregadas.includes(value)) {
+        listCondicionesAgregadas.push(value);
+        listCondicionesAgregadosObject.push({
+            idCondicion: value
+        })
         spanTituloCondicion.textContent = textoSeleccionado;
         divPadreCondicion.appendChild(spanTituloCondicion)
         divPadreCondicion.classList.add = "d-flex w-100 align-items-center justify-content-between";
         listadoCondicionesSeleccionadas.appendChild(divPadreCondicion)
     }    
+})
+
+//----------SERVICIOS DEPTO---------------
+
+window.addEventListener('load', async (event) => {
+    const respuesta = await fetch("http://localhost:8085/mantenedor/listar-servicios-depto");
+    if(respuesta.status === 200) {
+        serviciosDepto = await respuesta.json();
+        selectServiciosDepto = document.getElementById("select-servicio");
+        serviciosDepto.forEach(servicio => {
+            var option = document.createElement("option");
+            option.value = servicio.idServicioDepto;
+            option.text = servicio.tipoServicioDepto;
+            selectServiciosDepto.add(option)
+        })
+    }else if(respuesta.status === 401) {
+        console.log("error 401")
+    } else if(respuesta.status === 404) {
+        console.log("error 404")
+    }
+});
+
+var btnServicioDepto = document.getElementById("btnServicioDepto");
+var listadoServiciosDeptoSeleccionados = document.getElementById("list-servicios-depto");
+btnServicioDepto.addEventListener('click', function(){
+
+    var value = selectServiciosDepto.value;
+    // guarda el texto seleccionado del select
+    var textoSeleccionado = selectServiciosDepto.options[selectServiciosDepto.selectedIndex].text;
+    //text = "prohibdo fumar"
+    var divPadreServicioDepto = document.createElement("div")
+    var spanTituloServicioDepto = document.createElement("span")
+    
+    if(textoSeleccionado !== "Seleccione servicio" && !listServiciosAgregadosDepto.includes(value)) {
+        listServiciosAgregadosDepto.push(value);
+        listServiciosAgregadosObject.push({
+            idServicioDepto: value
+        })
+        spanTituloServicioDepto.textContent = textoSeleccionado;
+        divPadreServicioDepto.appendChild(spanTituloServicioDepto)
+        divPadreServicioDepto.classList.add = "d-flex w-100 align-items-center justify-content-between";
+        listadoServiciosDeptoSeleccionados.appendChild(divPadreServicioDepto)
+    }    
+})
+
+var btnCargarFoto = document.getElementById("btn-cargar-foto");
+var tableImagenesDepto = document.getElementById("table-body-imagenes-depto")
+btnCargarFoto.addEventListener('click', async function(){
+    // cargar imagen
+    const userFile = document.getElementById('formFile').files[0];
+    const tituloImagen = document.getElementById('titulo-imagen').value;
+    var trForImage = document.createElement("tr");
+    var tdTitle = document.createElement("td")
+    var tdImagen = document.createElement("td")
+    var imgForImagen = document.createElement("img")
+    var image64 = await toBase64(userFile);
+
+    imgForImagen.src = image64
+    imgForImagen.width = 100
+    imgForImagen.height = 100
+
+    tdTitle.textContent = tituloImagen
+    
+    var fotoEnviar = {
+        "tituloFotoDepto": tituloImagen,
+        "fotoDepto": image64
+    }
+
+    tdImagen.appendChild(imgForImagen);
+    trForImage.appendChild(tdTitle)
+    trForImage.appendChild(tdImagen)
+    tableImagenesDepto.appendChild(trForImage)
+    fotoDeptoList.push(fotoEnviar);
+    console.log(fotoDeptoList)
 })
