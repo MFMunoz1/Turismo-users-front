f59ae5642101bc0619aa221b086e7da2ca9a3d1d
osvaldoleiva <osvaldo.leiva@mercadolibre.cl>
2022-09-21 23:47:42 -0300

    fix crearServicioExtra

--- admin/admin/js/crearServicioExtra.js
+++ admin/admin/js/crearServicioExtra.js
@@ -1,63 +1,37 @@
 var selectTipoServicioExtra;
 selectTipoServicioExtra= document.getElementById('selectServicioExtra');
 
-console.log(selectTipoServicioExtra.text);
+console.log(selectTipoServicioExtra.value);
 // var IdServicioExtra = selectTipoServicioExtra.value;
 
 var form = document.getElementById('formularioCrearServicioExtra');
 
-if(selectTipoServicioExtra.value =="1" ){
-    form.addEventListener('submit', async function(e){
-        e.preventDefault();
-        var formData = new FormData(form);
-        // añadiendo lista de serivicion a object plain 
-        const plainObjectFormData = Object.fromEntries(formData.entries());
-        
-    
-        fetch("http://localhost:8085/servicio-extra/crear-servicio-extra-tour",
-        {
-        
-           body: JSON.stringify(plainObjectFormData),
-           method: "post",
-           mode: "cors",
-           headers: {
-               'Access-Control-Allow-Origin': '*',
-               "Content-Type": "application/json",
-               "Accept": "application/json"
-            }
-        }).then(
-            console.log("200")
-        );
-        
-        // Dont submit the form.
-        return false; 
-    });
-}
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    var formData = new FormData(form);
+    const plainObjectFormData = Object.fromEntries(formData.entries());
+    var endpoint = "";
+    if (selectTipoServicioExtra.value == "1") {
+        endpoint="crear-servicio-extra-tour"
+    }else if (selectTipoServicioExtra.value == "2"){
+        endpoint="crear-servicio-extra-transporte"
+    }else {
+        console.log("se selecciono un value invalido")
+        return false;
+    }
 
-else if(selectTipoServicioExtra.value =="2" ){
-    form.addEventListener('submit', async function(e){
-        e.preventDefault();
-        var formData = new FormData(form);
-        // añadiendo lista de serivicion a object plain 
-        const plainObjectFormData = Object.fromEntries(formData.entries());
+    console.log(JSON.stringify(plainObjectFormData))
+
+    var fullEndpoint = "http://localhost:8085/servicio-extra/" + endpoint 
+    fetch(fullEndpoint,{
+        body: JSON.stringify(plainObjectFormData),
+        method: "post",
+        mode: "cors",
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+         }
+    }).then(console.log("200"))
         
-    
-        fetch("http://localhost:8085/servicio-extra/crear-servicio-extra-transporte",
-        {
-        
-           body: JSON.stringify(plainObjectFormData),
-           method: "post",
-           mode: "cors",
-           headers: {
-               'Access-Control-Allow-Origin': '*',
-               "Content-Type": "application/json",
-               "Accept": "application/json"
-            }
-        }).then(
-            console.log("200")
-        );
-        
-        // Dont submit the form.
-        return false; 
-    });
-}
+})
