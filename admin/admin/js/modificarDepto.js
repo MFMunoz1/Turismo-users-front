251a121fd8f2e27d686c74659ed17e88f8444d78
osvaldoleiva <osvaldo.leiva@mercadolibre.cl>
2022-09-22 02:16:49 -0300

    boton eliminar deptos done

--- admin/admin/js/modificarDepto.js
+++ admin/admin/js/modificarDepto.js
@@ -10,11 +10,28 @@ fetch(url)
             console.log(data);
             let body = ""
             for(let i = 0; i< data.length; i++){
-                body += `<tr><td>${data[i].idDepartamento}</td><td>${data[i].direccion}</td><td>${data[i].comuna}</td><td>${data[i].region}</td><td>${data[i].estado}</td>
-                <td><button type="submit" class="btn btn-primary col-sm-12">Modificar</button></td>
-                <td><button type="submit" class="btn btn-danger col-sm-12">Eliminar</button></td></tr>`
+                body += `<tr><td value=${data[i].idDepartamento}>${data[i].idDepartamento}</td><td>${data[i].direccion}</td><td>${data[i].comuna}</td><td>${data[i].region}</td><td>${data[i].estado}</td>
+                <td><button class="btn btn-primary col-sm-12">Modificar</button></td>
+                <td><button class="btn btn-danger col-sm-12" name="delete">Eliminar</button></td></tr>`
             }
             document.getElementById('data').innerHTML = body
         }
 
 //FALTA AGREGAR FUNCIONALIDAD A BOTON DE MODIFICAR DEPTO
+document.getElementById("data").addEventListener('click', (e)=>{
+    console.log()
+    var idDepto = e.target.parentElement.parentElement.firstChild.textContent
+    var ruta = "http://localhost:8085/departamento/eliminar-depto?id_depto=" + idDepto;
+    fetch(ruta, {
+    method: "DELETE",
+    mode: "cors",
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+     }
+    }).then(
+        e.target.parentElement.parentElement.remove()
+    )
+
+})
